f12e6ac24112eaf9bc5d6982f76f1fb7bb61af08
Tait Brown <taitbrown@gmail.com>
2020-10-04 22:31:34 +1100

    ADDED: Modal image picker stub

--- src/components/sidebar/pickers/ImagePicker.js
+++ src/components/sidebar/pickers/ImagePicker.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import { Image } from '@chakra-ui/core';
+import {
+    Button,
+    Image,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/core';
 
 const ImagePicker = ({ value }) => {
-    return <Image src={value} width={100} />
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <>
+            <Image src={value} width={100} />
+
+            <Button onClick={onOpen}>Open Modal</Button>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Choose Image</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        djasdkhaksjdhaksdhkad jsdaks
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button variantColor="blue" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant="ghost">Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
 }
+
 
 export { ImagePicker }
