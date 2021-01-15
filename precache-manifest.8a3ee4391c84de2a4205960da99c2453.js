4f5ed703bc34870e09489cb5a5e77ed35ddb4e88
Tait Brown <taitbrown@gmail.com>
2021-01-15 16:21:22 +1100

    Updates

---
+++ precache-manifest.8a3ee4391c84de2a4205960da99c2453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f84f931e09f2430172726e65ee9290a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1a0d5ad88f529e54f6c",
+    "url": "/static/css/main.02a07afa.chunk.css"
+  },
+  {
+    "revision": "cebd852d8f5f9dfc277c",
+    "url": "/static/js/2.3a268d8f.chunk.js"
+  },
+  {
+    "revision": "1df2f64c4884cc731b5f10b0f115c143",
+    "url": "/static/js/2.3a268d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a0d5ad88f529e54f6c",
+    "url": "/static/js/main.7f5b1e23.chunk.js"
+  },
+  {
+    "revision": "d5de15dab594c2a6fc4c",
+    "url": "/static/js/runtime-main.6cd22a01.js"
+  }
+]);
